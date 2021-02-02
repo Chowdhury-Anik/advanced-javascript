040c5da20360515b9eb0ea9c0bced7e93e374bb5
Chowdhury-Anik <blogeranik1986@gmail.com>
2021-02-03 02:01:43 +0600

    undefine

---
+++ undefine.js
@@ -0,0 +1,16 @@
+// let pakhi;
+// console.log(pakhi);
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// const result = add(12 + 15);
+// console.log(result);
+
+// here is another way to find undefine number
+
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+const result = add(13, 12);
+console.log(result);
